1b68791472c609e466fecd20446214a91b7f442d
muammarhafiz <muammarhafiz@gmail.com>
2025-09-26 21:52:41 +0800

    Update workshop.ts

--- src/src/config/workshop.ts
+++ src/src/config/workshop.ts
@@ -1,1 +1,8 @@
 
+// src/config/workshop.ts
+// Edit these numbers in GitHub when your workshop moves.
+export const WORKSHOP = {
+  lat: 2.952838135391193,   // <-- your workshop latitude
+  lon: 101.73131632626544,  // <-- your workshop longitude
+  radiusM: 120,             // allowed radius in meters
+};
