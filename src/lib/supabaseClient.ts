a0c548c1e1f25d02856da1fa7ee683dbec7f3025
muammarhafiz <muammarhafiz@gmail.com>
2025-09-27 11:01:06 +0800

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,12 +1,10 @@
-// src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,          // e.g. https://abcxyz.supabase.co
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,     // anon public key
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
     auth: {
-      // make sure the session is stored on the browser and kept fresh
       persistSession: true,
       autoRefreshToken: true,
       detectSessionInUrl: true,
