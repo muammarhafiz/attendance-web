091c354dd94da7dcff136480979d0c54f8b9355f
muammarhafiz <muammarhafiz@gmail.com>
2025-10-02 07:25:10 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+
+
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -7,7 +9,6 @@ import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import PageShell from '../components/PageShell';
 import { Card, CardBody } from '../components/ui/Card';
-import Button from '../components/ui/Button';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -28,15 +29,8 @@ export default function HomePage() {
     <PageShell title="Workshop Attendance" subtitle={now}>
       <Card>
         <CardBody className="p-0">
-          {/* Map section */}
           <div className="h-[360px] w-full">
             <Map />
-          </div>
-
-          {/* Action buttons */}
-          <div className="flex justify-center gap-4 py-4 border-t">
-            <Button variant="primary">Check In</Button>
-            <Button variant="secondary">Check Out</Button>
           </div>
         </CardBody>
       </Card>
