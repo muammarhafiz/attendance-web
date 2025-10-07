75a000746577fdde2f49f6b1e256928e4340fc4b
muammarhafiz <muammarhafiz@gmail.com>
2025-10-07 20:01:10 +0800

    Update route.ts

--- src/app/salary/api/run/route.ts
+++ src/app/salary/api/run/route.ts
@@ -161,8 +161,7 @@ type StaffRow = {
 export async function POST() {
   try {
     // ⬇⬇ change this line
--   const cookieStore = cookies();
-+   const cookieStore = await cookies();
+   const cookieStore = await cookies();
 
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL as string,
@@ -170,8 +169,7 @@ export async function POST() {
       {
         cookies: {
           get(name: string) {
--           return cookieStore.get(name)?.value;
-+           return cookieStore.get(name)?.value;
+           return cookieStore.get(name)?.value;
           },
           set(name: string, value: string, options: CookieOptions) {
             /* noop in route handlers */
