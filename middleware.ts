d1634ec58c91575b209326c228efde73637b4ead
muammarhafiz <muammarhafiz@gmail.com>
2025-10-11 16:58:59 +0800

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+// middleware.ts
+import { createMiddlewareClient } from '@supabase/ssr'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+  await supabase.auth.getSession() // refresh cookie if expired
+  return res
+}
