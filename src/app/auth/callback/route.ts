e8808c874d2ab82b95dea3a302699232a2c06c0e
muammarhafiz <muammarhafiz@gmail.com>
2025-10-11 23:03:47 +0800

    Update route.ts

--- src/app/auth/callback/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-
-export async function GET(req: Request) {
-  const supabase = createRouteHandlerClient({ cookies })
-  await supabase.auth.exchangeCodeForSession(req.url)
-  return NextResponse.redirect(new URL('/', req.url))
-}
